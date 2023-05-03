d509707d6b38bc5eae6de261b59b1e7de3cfe882
Derek M <18333952+doolbneerg@users.noreply.github.com>
2023-05-03 12:08:43 -0700

    Making the data type names more verbose. Formatting the documents.

--- components/monoprice_10761/monoprice_10761.cpp
+++ components/monoprice_10761/monoprice_10761.cpp
@@ -25,44 +25,55 @@ static const int BUF_SIZE = 128;
 using namespace esphome;
 using namespace monoprice_10761;
 
-void Monoprice10761::setup() {
+void Monoprice10761::setup()
+{
     this->serial_read_buf_.reserve(BUF_SIZE);
 
     this->update();
 }
 
-void Monoprice10761::loop() {
+void Monoprice10761::loop()
+{
     this->read_from_rs232();
 }
 
-void Monoprice10761::update(){
-    uint8_t command[5] = {'?','1','0','\r','\n'};
-    for(uint8_t i = 0; i < this->zone_count_ / 6; i++){
-        ESP_LOGD(TAG,"Updating expansion %u...", i);
+void Monoprice10761::update()
+{
+    uint8_t command[5] = {'?', '1', '0', '\r', '\n'};
+    for (uint8_t i = 0; i < this->zone_count_ / 6; i++)
+    {
+        ESP_LOGD(TAG, "Updating expansion %u...", i);
         command[1] = '1' + i;
-        this->write_command(command, (uint8_t) 5);
+        this->write_command(command, (uint8_t)5);
     }
 }
 
-ZoneStatus* Monoprice10761::get_zone(uint8_t zone_id){
+ZoneStatus *Monoprice10761::get_zone(uint8_t zone_id)
+{
     uint8_t index = ZONE_ID_TO_INDEX(zone_id);
-    if(index >= this->zone_count_){
-        ESP_LOGE(TAG,"Index out of range for zone_id %u", zone_id);
+    if (index >= this->zone_count_)
+    {
+        ESP_LOGE(TAG, "Index out of range for zone_id %u", zone_id);
         return this->zones_[0];
     }
     return this->zones_[index];
 }
 
-void Monoprice10761::read_from_rs232() {
+void Monoprice10761::read_from_rs232()
+{
     int len;
-    while ((len = this->uart_->available()) > 0) {
+    while ((len = this->uart_->available()) > 0)
+    {
         uint8_t buf[len];
         this->uart_->read_array(buf, len);
-        for( size_t i = 0; i < len; i++){
+        for (size_t i = 0; i < len; i++)
+        {
             this->serial_read_buf_.push_back(buf[i]);
-            if(buf[i] == '#'){ // If we finished a line, flush it
+            if (buf[i] == '#')
+            { // If we finished a line, flush it
                 size_t line_len = this->serial_read_buf_.size();
-                if(line_len == 1) continue;
+                if (line_len == 1)
+                    continue;
 
                 // // Debug info
                 // char debug_buf[line_len-1];
@@ -78,72 +89,80 @@ void Monoprice10761::read_from_rs232() {
     }
 }
 
-char* get_command_as_string(const char* buf_input, size_t len_input){
-    char* buf = (char*) buf_input;
+char *get_command_as_string(const char *buf_input, size_t len_input)
+{
+    char *buf = (char *)buf_input;
     size_t len = len_input;
-    while(len > 0 &&
-        (
-            buf[0] == '#' ||
+    while (len > 0 &&
+           (buf[0] == '#' ||
             buf[0] == '\r' ||
             buf[0] == '\n' ||
-            buf[0] == ' '
-        )
-    ){
+            buf[0] == ' '))
+    {
         buf++;
         len--;
     }
 
-    while(len > 0 &&
-        (
-            buf[len-1] == '#' ||
-            buf[len-1] == '\r' ||
-            buf[len-1] == '\n' ||
-            buf[len-1] == ' '
-        )
-    ){
+    while (len > 0 &&
+           (buf[len - 1] == '#' ||
+            buf[len - 1] == '\r' ||
+            buf[len - 1] == '\n' ||
+            buf[len - 1] == ' '))
+    {
         len--;
     }
 
-    char* result = new char[len+1];
-    memcpy(result,buf,len);
-    result[len]=0;
+    char *result = new char[len + 1];
+    memcpy(result, buf, len);
+    result[len] = 0;
     return result;
 }
 
-
-void Monoprice10761::parse_command(const char* buf_input, size_t len_input) {
-    char* c_str = get_command_as_string(buf_input, len_input);
+void Monoprice10761::parse_command(const char *buf_input, size_t len_input)
+{
+    char *c_str = get_command_as_string(buf_input, len_input);
     size_t len = strlen(c_str);
 
     // ESP_LOGI(TAG,"Read: %s", c_str);
 
-    if(c_str[0] == '>' || c_str[0] == '<') {
+    if (c_str[0] == '>' || c_str[0] == '<')
+    {
         char _1 = c_str[1];
         char _2 = c_str[2];
         unsigned char zone = ((_1 - '0') * 10) + (_2 - '0');
-        ESP_LOGI(TAG,"Found zone status for %i", zone);
-        unsigned char zoneIndex = ((_1 - '1') * 6) + (_2-'1');
-        if(this->zone_count_ <= zoneIndex){
+        ESP_LOGI(TAG, "Found zone status for %i", zone);
+        unsigned char zoneIndex = ((_1 - '1') * 6) + (_2 - '1');
+        if (this->zone_count_ <= zoneIndex)
+        {
             ESP_LOGD(TAG, "Zone %i is out of range of configured zone count %i", zone, this->zone_count_);
-        } else {
-            ZoneStatus* status = this->zones_[zoneIndex];
-            if(c_str[0] == '>') {
+        }
+        else
+        {
+            ZoneStatus *status = this->zones_[zoneIndex];
+            if (c_str[0] == '>')
+            {
                 status->update(c_str);
-            } else {
-                ZoneStatusDataType type = ZoneStatus::str_to_data_type(c_str+3);
-                if(type == ZoneStatusDataType::UNKNOWN){
-                    ESP_LOGE(TAG, "Unknown attribute: %c%c", c_str[3],c_str[4]);
-                }else{
+            }
+            else
+            {
+                ZoneStatusDataType type = ZoneStatus::str_to_data_type(c_str + 3);
+                if (type == ZoneStatusDataType::UNKNOWN)
+                {
+                    ESP_LOGE(TAG, "Unknown attribute: %c%c", c_str[3], c_str[4]);
+                }
+                else
+                {
                     ESP_LOGD(TAG, "Updating %c%c", c_str[3], c_str[4]);
                     uint8_t data = TO_UINT8T(c_str, 5);
-                    status->update(type,data);
+                    status->update(type, data);
                 }
             }
             status->dump();
         }
     }
 
-    if(strcmp(c_str,"Command Error.") == 0) {
+    if (strcmp(c_str, "Command Error.") == 0)
+    {
         ESP_LOGE(TAG, "Command error received");
         this->errors_++;
     }
@@ -151,65 +170,73 @@ void Monoprice10761::parse_command(const char* buf_input, size_t len_input) {
     delete c_str;
 }
 
-void Monoprice10761::write_command(const uint8_t* cmd, size_t len) {
+void Monoprice10761::write_command(const uint8_t *cmd, size_t len)
+{
     this->uart_->write_array(cmd, len);
     this->read_from_rs232();
 }
 
-void Monoprice10761::dump_config() {
+void Monoprice10761::dump_config()
+{
     this->check_uart_settings(9600);
 }
 
-void ZoneStatus::update(char* zoneStatus){
+void ZoneStatus::update(char *zoneStatus)
+{
     const unsigned char base = 3;
-    this->update(ZoneStatusDataType::PA, TO_BOOL(zoneStatus, base+0));
-    this->update(ZoneStatusDataType::PR, TO_BOOL(zoneStatus, base+2));
-    this->update(ZoneStatusDataType::MU, TO_BOOL(zoneStatus, base+4));
-    this->update(ZoneStatusDataType::DT, TO_BOOL(zoneStatus, base+6));
-    this->update(ZoneStatusDataType::VO, TO_UINT8T(zoneStatus, base+8));
-    this->update(ZoneStatusDataType::TR, TO_UINT8T(zoneStatus, base+10));
-    this->update(ZoneStatusDataType::BS, TO_UINT8T(zoneStatus, base+12));
-    this->update(ZoneStatusDataType::BL, TO_UINT8T(zoneStatus, base+14));
-    this->update(ZoneStatusDataType::CH, TO_UINT8T(zoneStatus, base+16));
-    this->update(ZoneStatusDataType::LS, TO_BOOL(zoneStatus, base+18));
+    this->update(ZoneStatusDataType::PA, TO_BOOL(zoneStatus, base + 0));
+    this->update(ZoneStatusDataType::PR, TO_BOOL(zoneStatus, base + 2));
+    this->update(ZoneStatusDataType::MU, TO_BOOL(zoneStatus, base + 4));
+    this->update(ZoneStatusDataType::DT, TO_BOOL(zoneStatus, base + 6));
+    this->update(ZoneStatusDataType::VO, TO_UINT8T(zoneStatus, base + 8));
+    this->update(ZoneStatusDataType::TR, TO_UINT8T(zoneStatus, base + 10));
+    this->update(ZoneStatusDataType::BS, TO_UINT8T(zoneStatus, base + 12));
+    this->update(ZoneStatusDataType::BL, TO_UINT8T(zoneStatus, base + 14));
+    this->update(ZoneStatusDataType::CH, TO_UINT8T(zoneStatus, base + 16));
+    this->update(ZoneStatusDataType::LS, TO_BOOL(zoneStatus, base + 18));
 }
 
-void ZoneStatus::update(ZoneStatusDataType type, uint8_t val){
-    const uint8_t type_uint8 = (uint8_t) type;
-    if(this->data_[type_uint8] != val){
+void ZoneStatus::update(ZoneStatusDataType type, uint8_t val)
+{
+    const uint8_t type_uint8 = (uint8_t)type;
+    if (this->data_[type_uint8] != val)
+    {
         this->data_[type_uint8] = val;
         // Run through listeners
-        for (auto &listener : this->listeners_){
-            if (listener.data_type == type){
+        for (auto &listener : this->listeners_)
+        {
+            if (listener.data_type == type)
+            {
                 listener.on_update(val);
             }
         }
     }
 }
 
-void ZoneStatus::dump(){
+void ZoneStatus::dump()
+{
     ESP_LOGD(TAG, "ZoneStatus(%u): pa(%u) pr(%u) mu(%u) dt(%u) vo(%u) tr(%u) bs(%u) bl(%u) ch(%u) ls(%u)",
-        this->zone_,
-        this->data_[(uint8_t)ZoneStatusDataType::PA],
-        this->data_[(uint8_t)ZoneStatusDataType::PR],
-        this->data_[(uint8_t)ZoneStatusDataType::MU],
-        this->data_[(uint8_t)ZoneStatusDataType::DT],
-        this->data_[(uint8_t)ZoneStatusDataType::VO],
-        this->data_[(uint8_t)ZoneStatusDataType::TR],
-        this->data_[(uint8_t)ZoneStatusDataType::BS],
-        this->data_[(uint8_t)ZoneStatusDataType::BL],
-        this->data_[(uint8_t)ZoneStatusDataType::CH],
-        this->data_[(uint8_t)ZoneStatusDataType::LS]
-    );
+             this->zone_,
+             this->data_[(uint8_t)ZoneStatusDataType::PA],
+             this->data_[(uint8_t)ZoneStatusDataType::PR],
+             this->data_[(uint8_t)ZoneStatusDataType::MU],
+             this->data_[(uint8_t)ZoneStatusDataType::DT],
+             this->data_[(uint8_t)ZoneStatusDataType::VO],
+             this->data_[(uint8_t)ZoneStatusDataType::TR],
+             this->data_[(uint8_t)ZoneStatusDataType::BS],
+             this->data_[(uint8_t)ZoneStatusDataType::BL],
+             this->data_[(uint8_t)ZoneStatusDataType::CH],
+             this->data_[(uint8_t)ZoneStatusDataType::LS]);
 }
 
-void ZoneStatus::set(ZoneStatusDataType type, const unsigned char val){
+void ZoneStatus::set(ZoneStatusDataType type, const unsigned char val)
+{
     const int len = 9;
     uint8_t buf[len];
     buf[0] = '<';
-    buf[1] = '0' + this->zone_/10;
-    buf[2] = '0' + this->zone_%10;
-    const char* type_str = ZoneStatus::data_type_to_str(type);
+    buf[1] = '0' + this->zone_ / 10;
+    buf[2] = '0' + this->zone_ % 10;
+    const char *type_str = ZoneStatus::data_type_to_str(type);
     buf[3] = type_str[0];
     buf[4] = type_str[1];
     buf[5] = '0' + val / 10;
@@ -220,7 +247,8 @@ void ZoneStatus::set(ZoneStatusDataType type, const unsigned char val){
     this->send_command_(buf, len);
 }
 
-void ZoneStatus::register_listener(ZoneStatusDataType data_type, const std::function<void(uint8_t)> &func){
+void ZoneStatus::register_listener(ZoneStatusDataType data_type, const std::function<void(uint8_t)> &func)
+{
     auto listener = ZoneStatusListener{
         .data_type = data_type,
         .on_update = func,
--- components/monoprice_10761/monoprice_10761.h
+++ components/monoprice_10761/monoprice_10761.h
@@ -1,18 +1,18 @@
 /* Copyright (C) 2021 Johan Henkens
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -27,171 +27,189 @@
 #define INDEX_TO_ZONE_ID(index) ((((index / 6) + 1) * 10) + (index % 6) + 1)
 #define ZONE_ID_TO_INDEX(zone_id) (((zone_id / 10) - 1) * 6 + ((zone_id % 10) - 1))
 
-#define TO_UINT8T(ptr, offset) ((ptr[offset] - '0')*10 + (ptr[offset+1] -'0'))
-#define TO_BOOL(ptr, offset) ((bool) (ptr[offset+1] - '0'))
+#define TO_UINT8T(ptr, offset) ((ptr[offset] - '0') * 10 + (ptr[offset + 1] - '0'))
+#define TO_BOOL(ptr, offset) ((bool)(ptr[offset + 1] - '0'))
 
-namespace esphome {
-namespace monoprice_10761 {
+namespace esphome
+{
+    namespace monoprice_10761
+    {
+        enum class ZoneStatusDataType : uint8_t
+        {
+            PublicAnnouncement = 0x00, // PA mode - 00, 01
+            Power = 0x01,              // Power - 00, 01
+            Mute = 0x02,               // Mute - 00, 01
+            DoNotDisturb = 0x03,       // Do not disturb - 00, 01
+            Volume = 0x04,             // Volume - 00-38
+            Trebel = 0x05,             // Trebel - 00-14
+            Bass = 0x06,               // Bass - 00-14
+            Balance = 0x07,            // Balance - 00-20
+            Channel = 0x08,            // Channel - 01-06
+            KeypadStatus = 0x09,       // Keypad status - 00, 01
 
-enum class ZoneStatusDataType : uint8_t {
-    PA = 0x00,  // PA mode - 00, 01
-    PR = 0x01,  // Power - 00, 01
-    MU = 0x02,  // Mute - 00, 01
-    DT = 0x03,  // Do not disturb - 00, 01
-    VO = 0x04,  // Volume - 00-38
-    TR = 0x05,  // Trebel - 00-14
-    BS = 0x06,  // Bass - 00-14
-    BL = 0x07,  // Balance - 00-20
-    CH = 0x08,  // Channel - 01-06
-    LS = 0x09,  // Keypad status - 00, 01
+            UNKNOWN = 0x0a // keep it as the max value + 1
+        };
 
-    UNKNOWN = 0x0a // keep it as the max value + 1
-};
+        struct ZoneStatusListener
+        {
+            ZoneStatusDataType data_type;
+            std::function<void(uint8_t)> on_update;
+        };
 
-struct ZoneStatusListener {
-    ZoneStatusDataType data_type;
-    std::function<void(uint8_t)> on_update;
-};
+        class ZoneStatus
+        {
+        public:
+            ZoneStatus(int zone, const std::function<void(const uint8_t *, size_t)> &send_command)
+            {
+                this->zone_ = zone;
+                this->send_command_ = send_command;
+                for (uint8_t i = 0; i < ((uint8_t)ZoneStatusDataType::UNKNOWN); i++)
+                {
+                    this->data_[i] = (uint8_t)0;
+                }
+            }
+            void register_listener(ZoneStatusDataType datapoint_id, const std::function<void(uint8_t)> &func);
+            void update(char *zoneStatus);
+            void update(ZoneStatusDataType type, uint8_t val);
+            void dump();
 
-class ZoneStatus{
-public:
-    ZoneStatus(int zone, const std::function<void(const uint8_t*, size_t)> &send_command){
-        this->zone_ = zone;
-        this->send_command_ = send_command;
-        for(uint8_t i = 0; i < ((uint8_t)ZoneStatusDataType::UNKNOWN); i++){
-            this->data_[i] = (uint8_t)0;
-        }
-    }
-    void register_listener(ZoneStatusDataType datapoint_id, const std::function<void(uint8_t)> &func);
-    void update(char* zoneStatus);
-    void update(ZoneStatusDataType type, uint8_t val);
-    void dump();
+            unsigned char zone_;
+            void set(ZoneStatusDataType cmd, const unsigned char val);
+            void get(ZoneStatusDataType type);
 
-    unsigned char zone_;
-    void set(ZoneStatusDataType cmd, const unsigned char val);
-    void get(ZoneStatusDataType type);
+            static const char *data_type_to_str(ZoneStatusDataType type)
+            {
+                switch (type)
+                {
+                case ZoneStatusDataType::PublicAnnouncement:
+                    return "PA";
+                case ZoneStatusDataType::Power:
+                    return "PR";
+                case ZoneStatusDataType::Mute:
+                    return "MU";
+                case ZoneStatusDataType::DoNotDisturb:
+                    return "DT";
+                case ZoneStatusDataType::Volume:
+                    return "VO";
+                case ZoneStatusDataType::Trebel:
+                    return "TR";
+                case ZoneStatusDataType::Bass:
+                    return "BS";
+                case ZoneStatusDataType::Balance:
+                    return "BL";
+                case ZoneStatusDataType::Channel:
+                    return "CH";
+                case ZoneStatusDataType::KeypadStatus:
+                    return "LS";
+                default:
+                    return "UNKNOWN";
+                }
+            }
 
-    static const char* data_type_to_str(ZoneStatusDataType type){
-        switch(type){
-            case ZoneStatusDataType::PA:
-            return "PA";
-            case ZoneStatusDataType::PR:
-            return "PR";
-            case ZoneStatusDataType::MU:
-            return "MU";
-            case ZoneStatusDataType::DT:
-            return "DT";
-            case ZoneStatusDataType::VO:
-            return "VO";
-            case ZoneStatusDataType::TR:
-            return "TR";
-            case ZoneStatusDataType::BS:
-            return "BS";
-            case ZoneStatusDataType::BL:
-            return "BL";
-            case ZoneStatusDataType::CH:
-            return "CH";
-            case ZoneStatusDataType::LS:
-            return "LS";
-            default:
-            return "UNKNOWN";
-        }
-    }
+            static ZoneStatusDataType str_to_data_type(const char *data_type)
+            {
+                uint8_t first = data_type[0];
+                uint8_t second = data_type[1];
+                first = first > 'Z' ? (first - 'a') + 'A' : first;
+                second = second > 'Z' ? (second - 'a') + 'A' : second;
+                uint16_t attribute = first << 8 | second;
+                switch (attribute)
+                {
+                case 'P' << 8 | 'A':
+                    return ZoneStatusDataType::PublicAnnouncement;
+                case 'P' << 8 | 'R':
+                    return ZoneStatusDataType::Power;
+                case 'M' << 8 | 'U':
+                    return ZoneStatusDataType::Mute;
+                case 'D' << 8 | 'T':
+                    return ZoneStatusDataType::DoNotDisturb;
+                case 'V' << 8 | 'O':
+                    return ZoneStatusDataType::Volume;
+                case 'T' << 8 | 'R':
+                    return ZoneStatusDataType::Trebel;
+                case 'B' << 8 | 'S':
+                    return ZoneStatusDataType::Bass;
+                case 'B' << 8 | 'L':
+                    return ZoneStatusDataType::Balance;
+                case 'C' << 8 | 'H':
+                    return ZoneStatusDataType::Channel;
+                case 'L' << 8 | 'S':
+                    return ZoneStatusDataType::KeypadStatus;
+                default:
+                    return ZoneStatusDataType::UNKNOWN;
+                }
+            }
 
-    static ZoneStatusDataType str_to_data_type(const char* data_type){
-        uint8_t first = data_type[0];
-        uint8_t second = data_type[1];
-        first = first > 'Z' ? (first - 'a') + 'A' : first;
-        second = second > 'Z' ? (second - 'a') + 'A' : second;
-        uint16_t attribute = first << 8 | second;
-        switch(attribute){
-            case 'P' << 8 | 'A':
-            return ZoneStatusDataType::PA;
-            case 'P' << 8 | 'R':
-            return ZoneStatusDataType::PR;
-            case 'M' << 8 | 'U':
-            return ZoneStatusDataType::MU;
-            case 'D' << 8 | 'T':
-            return ZoneStatusDataType::DT;
-            case 'V' << 8 | 'O':
-            return ZoneStatusDataType::VO;
-            case 'T' << 8 | 'R':
-            return ZoneStatusDataType::TR;
-            case 'B' << 8 | 'S':
-            return ZoneStatusDataType::BS;
-            case 'B' << 8 | 'L':
-            return ZoneStatusDataType::BL;
-            case 'C' << 8 | 'H':
-            return ZoneStatusDataType::CH;
-            case 'L' << 8 | 'S':
-            return ZoneStatusDataType::LS;
-            default:
-            return ZoneStatusDataType::UNKNOWN;
-        }
-    }
+        private:
+            uint8_t data_[((uint8_t)ZoneStatusDataType::UNKNOWN)];
+            std::function<void(const uint8_t *, size_t)> send_command_;
+            std::vector<ZoneStatusListener> listeners_;
+        };
 
-private:
-    uint8_t data_[((uint8_t)ZoneStatusDataType::UNKNOWN)];
-    std::function<void(const uint8_t*, size_t)> send_command_;
-    std::vector<ZoneStatusListener> listeners_;
-};
+        class Monoprice10761 : public esphome::PollingComponent, public uart::UARTDevice
+        {
+        public:
+            Monoprice10761() : PollingComponent(15000) {}
+            explicit Monoprice10761(esphome::uart::UARTComponent *uart) : PollingComponent(15000), uart_{uart} {}
 
-class Monoprice10761 : public esphome::PollingComponent, public uart::UARTDevice  {
-public:
-    Monoprice10761() : PollingComponent(15000) {}
-    explicit Monoprice10761(esphome::uart::UARTComponent *uart) : PollingComponent(15000), uart_{uart} {}
+            void setup() override;
+            void loop() override;
+            void update() override;
+            void dump_config() override;
 
-    void setup() override;
-    void loop() override;
-    void update() override;
-    void dump_config() override;
+            void write_command(const uint8_t *cmd, size_t len);
 
-    void write_command(const uint8_t* cmd, size_t len);
+            ZoneStatus *get_zone(uint8_t zone_id);
 
-    ZoneStatus* get_zone(uint8_t zone_id);
+            float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
 
-    float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
+            void set_uart_parent(esphome::uart::UARTComponent *parent)
+            {
+                this->uart_ = parent;
+                uart::UARTDevice::set_uart_parent(parent);
+            }
 
-    void set_uart_parent(esphome::uart::UARTComponent *parent) {
-        this->uart_ = parent;
-        uart::UARTDevice::set_uart_parent(parent);
-    }
+            void set_expansions(uint16_t expansions)
+            {
+                this->zone_count_ = (expansions + 1) * 6;
+                this->zones_ = new ZoneStatus *[this->zone_count_];
+                for (unsigned char i = 0; i < this->zone_count_; i++)
+                {
+                    this->zones_[i] = new ZoneStatus(INDEX_TO_ZONE_ID(i), [this](const uint8_t *buf, size_t len)
+                                                     { this->write_command(buf, len); });
+                }
+            }
 
-    void set_expansions(uint16_t expansions) {        
-        this->zone_count_ = (expansions+1)*6;
-        this->zones_ = new ZoneStatus*[this->zone_count_];
-        for(unsigned char i = 0; i < this->zone_count_; i++){
-            this->zones_[i] = new ZoneStatus(INDEX_TO_ZONE_ID(i), [this](const uint8_t* buf, size_t len){this->write_command(buf,len);});
-        }
-    }
+            void set_input(uint8_t num, const char *name, bool hide)
+            {
+                this->inputs_[num - 1].number = num;
+                this->inputs_[num - 1].name = std::string(name);
+                this->inputs_[num - 1].hide = hide;
+            }
 
-    void set_input(uint8_t num, const char* name, bool hide){
-        this->inputs_[num-1].number = num;
-        this->inputs_[num-1].name = std::string(name);
-        this->inputs_[num-1].hide = hide;
-    }
+            struct Input
+            {
+                uint8_t number;
+                std::string name;
+                bool hide;
+            };
 
-    struct Input{
-        uint8_t number;
-        std::string name;
-        bool hide;
-    };
+            Input inputs_[6];
 
-    Input inputs_[6];
+        protected:
+            void cleanup();
+            void read_from_rs232();
 
-protected:
-    void cleanup();
-    void read_from_rs232();
+            unsigned char zone_count_{0};
+            unsigned int errors_{0};
+            ZoneStatus **zones_{0};
+            esphome::uart::UARTComponent *uart_{nullptr};
+            int send_client_{0};
+            std::vector<char> serial_read_buf_{};
 
-    unsigned char zone_count_{0};
-    unsigned int errors_{0};
-    ZoneStatus** zones_{0};
-    esphome::uart::UARTComponent *uart_{nullptr};
-    int send_client_{0};
-    std::vector<char> serial_read_buf_{};
-private:
-    void parse_command(const char* buf, size_t len);
-};
+        private:
+            void parse_command(const char *buf, size_t len);
+        };
 
-} // namespace monoprice_10761
+    } // namespace monoprice_10761
 } // namespace esphome
